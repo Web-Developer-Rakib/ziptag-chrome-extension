3b0386b587a15894bfa93b1ec164f10c065f8cf8
Web-Developer-Rakib <rakibodesk7@gmail.com>
2023-11-09 22:25:29 -0600

    Detected the registration pages

--- scripts/content.js
+++ scripts/content.js
@@ -1,21 +1,32 @@
+// Function to check if the current URL contains specific keywords
+function isRegistrationPage() {
+  const keywords = ["register", "registration", "signup"];
+  const currentURL = window.location.href.toLowerCase();
+
+  return keywords.some((keyword) => currentURL.includes(keyword));
+}
+
 // Function to add an extra field and a button inside the form
 function adduuidFieldAndButton(form) {
-  // Create a new input field
-  const uuidField = document.createElement("input");
-  uuidField.setAttribute("type", "text");
-  uuidField.setAttribute("name", "uuidField");
-  uuidField.setAttribute("placeholder", "Enter the uuid");
+  // Check if the current page is a registration page
+  if (isRegistrationPage()) {
+    // Create a new input field
+    const uuidField = document.createElement("input");
+    uuidField.setAttribute("type", "text");
+    uuidField.setAttribute("name", "uuidField");
+    uuidField.setAttribute("placeholder", "Enter the uuid");
 
-  // Create a new button
-  const submitButton = document.createElement("button");
-  submitButton.innerHTML = "Use zip tag";
-  submitButton.addEventListener("click", function () {
-    alert("Button Clicked!");
-  });
+    // Create a new button
+    const submitButton = document.createElement("button");
+    submitButton.innerHTML = "Use zip tag";
+    submitButton.addEventListener("click", function () {
+      alert(uuidField.value);
+    });
 
-  // Add the new field and button to the form
-  form.appendChild(uuidField);
-  form.appendChild(submitButton);
+    // Add the new field and button to the form
+    form.appendChild(uuidField);
+    form.appendChild(submitButton);
+  }
 }
 
 // Find and modify forms on the page
